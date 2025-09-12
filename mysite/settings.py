d80f8c7f9f998ccbe2e74ce53418df665006ced5
duythinh26 <pduythinh79@gmail.com>
2025-09-13 00:20:59 +0700

    create basic polls app

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Include the Polls app by adding the following line:
     "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
