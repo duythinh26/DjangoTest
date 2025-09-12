2e1af256305ad70d10b7a7029d50582f948c5284
duythinh26 <pduythinh79@gmail.com>
2025-09-12 22:22:30 +0700

    create models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    # A Question can have many Choices, but each Choice belongs to only one Question.
+    # The on_delete=models.CASCADE rule ensures data integrity. There is no such thing as a Choice that doesn't belong to any question.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    # Return the contents of the "question_text" field as a wildcard.
+    def __str__(self):
+        return self.choice_text
