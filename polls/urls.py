0af0dc49c1b4045a8d728a2e31aa4f66ac1efc3e
duythinh26 <pduythinh79@gmail.com>
2025-09-13 23:50:46 +0700

    add more api for polls app

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 
 from . import views
 
+# add an app_name to help with namespacing
+app_name = "polls"
+
 urlpatterns = [
     # Create a URL pattern for the index view
     # The empty string "" represents the root URL of the app
     # views.index is the index function we defined in views.py
     # name="index" gives the URL pattern a name that can be used for reverse URL
-    path("", views.index, name="index"),
+    path("", views.index, name="index"), # ex: /polls/
+    
+    path("specifics/<int:question_id>/", views.detail, name="detail"), # ex: /polls/5/
+    
+    path("<int:question_id>/results/", views.results, name="results"), # ex: /polls/5/results/
+    
+    path("<int:question_id>/vote/", views.vote, name="vote"), # ex: /polls/5/vote/
 ]
