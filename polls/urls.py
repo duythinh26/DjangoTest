400c91926ea99b83ebbfd5640a55ea34acfa7a50
duythinh26 <pduythinh79@gmail.com>
2025-09-14 14:52:57 +0700

    change to Class View instead of Function View

--- polls/urls.py
+++ polls/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     # The empty string "" represents the root URL of the app
     # views.index is the index function we defined in views.py
     # name="index" gives the URL pattern a name that can be used for reverse URL
-    path("", views.index, name="index"), # ex: /polls/
+    path("", views.IndexView.as_view(), name="index"), # ex: /polls/
     
     # the 'name' value as called by the {% url %} template tag
-    path("<int:question_id>/", views.detail, name="detail"), # ex: /polls/5/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"), # ex: /polls/5/
     
-    path("<int:question_id>/results/", views.results, name="results"), # ex: /polls/5/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"), # ex: /polls/5/results/
     
     path("<int:question_id>/vote/", views.vote, name="vote"), # ex: /polls/5/vote/
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,29 +6,31 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
 # Each view function takes at least one argument, typically a request argument.
 # Each view is responsible for doing one of two things: returning an HttpResponse object containing the content for the requested page, or raising an exception such as Http404
-def index(request):
-    # .order_by("-pub_date"): Sort questions by posting date, minus sign (-) means from newest to oldest.
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    
-    #Create a "context" - this is a dictionary to pass data from the view to the template.
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    # Using template_name to specify the template to use
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
